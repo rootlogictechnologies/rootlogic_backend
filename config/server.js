9a0775ea9a5b234e265a7dd16652f4c09476ef73
Kirtika Malhotra <malhotra.kirtika@gmail.com>
2022-02-18 01:04:12 +0530

    admin strapi url enabled

--- config/server.js
+++ config/server.js
@@ -1,4 +1,17 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+// });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "http://api.rootlogic.tech",
 });
+
+// path: ./config/admin.js
+
+// module.exports = ({ env }) => ({
+//   url: "/", // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+//   serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
+// });
