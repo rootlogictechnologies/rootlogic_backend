0789058a278fce48d7a8b2e07b43c9ba8f00837a
Kirtika Malhotra <malhotra.kirtika@gmail.com>
2022-02-19 20:34:24 +0530

    priority added

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,12 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'fb976434cf30ca34f2cd82fceda5f912'),
+    secret: env("ADMIN_JWT_SECRET", "fb976434cf30ca34f2cd82fceda5f912"),
   },
 });
+
+// path: ./config/admin.js
+
+// module.exports = ({ env }) => ({
+//   url: "/", // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+//   serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
+// });
