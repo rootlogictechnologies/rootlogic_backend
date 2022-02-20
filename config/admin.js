ba6dc043aafb7222116a11dbfd7481d2b4ba41f8
Kirtika Malhotra <malhotra.kirtika@gmail.com>
2022-02-20 21:06:32 +0530

    docker file, admin config updated

--- config/admin.js
+++ config/admin.js
@@ -1,12 +1,13 @@
+// module.exports = ({ env }) => ({
+//   auth: {
+//     secret: env("ADMIN_JWT_SECRET", "fb976434cf30ca34f2cd82fceda5f912"),
+//   },
+// });
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET", "fb976434cf30ca34f2cd82fceda5f912"),
   },
+  url: "/admin", // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+  serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
 });
-
-// path: ./config/admin.js
-
-// module.exports = ({ env }) => ({
-//   url: "/", // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
-//   serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
-// });
