4352e0643f56526d2c7f7bce7a77fa33de6b47d3
Kirtika Malhotra <malhotra.kirtika@gmail.com>
2022-02-20 21:26:00 +0530

    undo changes

--- config/admin.js
+++ config/admin.js
@@ -8,6 +8,6 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET", "fb976434cf30ca34f2cd82fceda5f912"),
   },
-  url: "/admin", // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
-  serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
+  // url: "/admin", // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+  // serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
 });
